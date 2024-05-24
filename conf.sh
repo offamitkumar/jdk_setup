e5c37fd3f020e24deb1aeff2835047c97f8c2264
Amit Kumar <amitlocham09@icloud.com>
2024-05-24 08:38:39 +0000

    conf-var

--- conf.sh
+++ conf.sh
@@ -1,12 +1,25 @@
+
+if [ "$CONF" = "linux-s390x-server-fastdebug" ]; then
+  DEBUG_LEVEL=fastdebug
+fi
+
+if [ "$CONF" = "linux-s390x-server-release" ]; then
+  DEBUG_LEVEL=release
+fi
+
+if [ "$CONF" = "linux-s390x-server-slowdebug" ]; then
+  DEBUG_LEVEL=slowdebug
+fi
+
 bash configure \
 	--with-boot-jdk=boot_jdk_ \
 	--with-jtreg=jtreg \
 	--with-gtest=googletest \
-	--with-debug-level=release \
+	--with-jmh=build/jmh/jars \
+	--with-debug-level=$DEBUG_LEVEL \
 	--with-native-debug-symbols=internal \
 	--disable-precompiled-headers
 
     #--with-jvm-variants=fastdebug \
-    #--with-jmh=build/jmh/jars \
     # sh make/devkit/createJMHBundle.sh
     # --enable-ubsan \
